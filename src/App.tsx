4431693879722eb3f80b27e92dc23e53d949dde4
Dawood Ahmad <dawood11@hotmail.com>
2022-04-14 23:19:28 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import { initializeApp } from "firebase/app";
 const App: React.FC = () => {
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAFCwlk9IPhBaa-iA36NM_nkgDS7T8sD44",
-    authDomain: "dawood-dashboard.firebaseapp.com",
-    projectId: "dawood-dashboard",
-    storageBucket: "dawood-dashboard.appspot.com",
-    messagingSenderId: "680565836471",
-    appId: "1:680565836471:web:29d5ecb44e986bb2495b32"
-  };
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyAFCwlk9IPhBaa-iA36NM_nkgDS7T8sD44",
+  //   authDomain: "dawood-dashboard.firebaseapp.com",
+  //   projectId: "dawood-dashboard",
+  //   storageBucket: "dawood-dashboard.appspot.com",
+  //   messagingSenderId: "680565836471",
+  //   appId: "1:680565836471:web:29d5ecb44e986bb2495b32"
+  // };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  // const app = initializeApp(firebaseConfig);
 
   return (
     <div className="App">
-
+      TEST
     </div>
   );
 }
