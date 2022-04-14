782a233670ac121e6daeef52317f517e68c1bad1
Dawood Ahmad <dawood11@hotmail.com>
2022-04-14 23:22:47 +0200

    build

---
+++ build/precache-manifest.e604b7f1cdc6202f540a73430fb904fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff243c71f221b4157f8e",
+    "url": "/static/css/main.04588b50.chunk.css"
+  },
+  {
+    "revision": "ff243c71f221b4157f8e",
+    "url": "/static/js/main.ff243c71.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "e495a233d13d271953c2",
+    "url": "/static/js/2.e495a233.chunk.js"
+  },
+  {
+    "revision": "dc951fff484053e1c299",
+    "url": "/static/js/runtime~main.dc951fff.js"
+  },
+  {
+    "revision": "7d8b38a5bb9bdedbf9dea2f10f49820d",
+    "url": "/index.html"
+  }
+];
